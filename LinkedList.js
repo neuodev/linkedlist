179a63452ad176d306b2864b7ed9ae3cc28c321f
Ahmed Ibrahim <ib153507@gmail.com>
2021-03-27 07:36:14 +0200

    ADD the addFirst method

--- LinkedList.js
+++ LinkedList.js
@@ -21,10 +21,23 @@ class LinkedList {
     }
     return newNode;
   }
+
+  addFirst(node) {
+    const newNode = new Node(node);
+    if (this.head === null || this.tail === null) {
+      this.head = newNode;
+      this.tail = this.head;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    return newNode;
+  }
 }
 
 const LL = new LinkedList();
 LL.addLast(0);
 LL.addLast(1);
 LL.addLast(2);
+LL.addFirst(-1)
 console.log(LL);
