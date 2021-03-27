b83bf11149f0ffc923d3fcb9f19163b767149f82
Ahmed Ibrahim <ib153507@gmail.com>
2021-03-27 07:56:11 +0200

    ADD contains method

--- LinkedList.js
+++ LinkedList.js
@@ -1,6 +1,6 @@
 class Node {
   constructor(node) {
-    this.node = node;
+    this.val = node;
     this.next = null;
   }
 }
@@ -48,7 +48,45 @@ class LinkedList {
       this.head = this.head.next;
     }
     this.length--;
+    head.next = null;
     return head;
+  }
+
+  deleteLast() {
+    if (this.head === null || this.tail === null) {
+      return -1;
+    }
+    let node;
+    if (this.head.val === this.tail.val) {
+      node = this.head;
+      this.head = null;
+      this.tail = null;
+    } else {
+      let currentNode = this.head;
+      while (currentNode.next && currentNode.next.next) {
+        currentNode = currentNode.next;
+      }
+      // store the deleted node to return it
+      node = currentNode.next;
+      currentNode.next = null;
+      this.tail = currentNode;
+    }
+    this.length--;
+    return node;
+  }
+  contains(val) {
+    if (this.head === null || this.tail === null) {
+      return -1;
+    }
+    // Traverse the Linked list untill we reach the tree
+    let currentNode = this.head;
+    let idx = 0;
+    while (currentNode) {
+      if (currentNode.val === val) return idx;
+      currentNode = currentNode.next;
+      idx++;
+    }
+    return -1;
   }
 }
 
@@ -56,5 +94,6 @@ const LL = new LinkedList();
 LL.addLast(0);
 LL.addLast(1);
 LL.addLast(2);
-LL.addFirst(-1);
-console.log(LL);
+LL.addLast(3);
+LL.addLast(4);
+console.log(LL.contains(4));
